f5aa67f840c89d4a6121179ad337b55815c53b39
tieptv6669 <tieptv6669@gmail.com>
2018-10-26 09:15:45 +0700

    Tiep commit 2

--- Group1/src/main/java/com/studentmanager/controller/HomeController.java
+++ Group1/src/main/java/com/studentmanager/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.studentmanager.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.studentmanager.entity.Diem;
+import com.studentmanager.service.DiemRepository;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private DiemRepository diemRepository;
+	
 	@RequestMapping("/")
 	public String defaul() {
 		return "Home";
@@ -19,8 +30,13 @@ public class HomeController {
 	public String getClasses() {
 		return "Classes";
 	}
-	@RequestMapping(value = "/point", method = RequestMethod.POST)
-	public String getPoint() {
+	@RequestMapping(value = "/point", method = RequestMethod.GET)
+	public String getPoints(Model model) {
+		List<Diem> diems = new ArrayList<Diem>();
+		diems = diemRepository.findAll();
+		model.addAttribute("pointList", diems);
+		
 		return "Point";
 	}
+	
 }
--- Group1/src/main/java/com/studentmanager/service/DiemRepository.java
+++ Group1/src/main/java/com/studentmanager/service/DiemRepository.java
@@ -2,11 +2,12 @@ package com.studentmanager.service;
 
 import java.util.List;
 
-import org.springframework.data.repository.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.studentmanager.entity.Diem;
 
-public interface DiemRepository extends Repository<Diem, String> {
+public interface DiemRepository extends JpaRepository<Diem, String> {
+	List<Diem> findAll();
 	List<Diem> findByMaSV(String maSV);
 	List<Diem> findByMaMH(String maMH);
 }
