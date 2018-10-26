e56f6c44b9815d3d01b958f1441763e0bb8b99e2
atomx91 <atomx91@gmail.com>
2018-10-26 10:46:18 +0700

    Dung commit

--- Group1/src/main/java/com/studentmanager/controller/HomeController.java
+++ Group1/src/main/java/com/studentmanager/controller/HomeController.java
@@ -1,38 +1,19 @@
 package com.studentmanager.controller;
 
-<<<<<<< HEAD
 import org.springframework.stereotype.Controller;
-=======
-import java.util.ArrayList;
-import java.util.List;
-
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-
->>>>>>> 64555b52f96d5c633a26c83a6d5ea68aa091ab16
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
-import com.studentmanager.entity.SinhVien;
-import com.studentmanager.service.SinhVienRepository;
+import com.studentmanager.service.DiemRepository;
 
 @Controller
 public class HomeController {
-<<<<<<< HEAD
-	
-	
-=======
+
 	@Autowired
 	private DiemRepository diemRepository;
 
->>>>>>> 64555b52f96d5c633a26c83a6d5ea68aa091ab16
 	@RequestMapping("/")
 	public String defaul() {
 		return "Home";
@@ -47,21 +28,5 @@ public class HomeController {
 	public String getClasses() {
 		return "Classes";
 	}
-<<<<<<< HEAD
-=======
 
-	@RequestMapping(value = "/point", method = RequestMethod.GET)
-	public String getPoints(Model model) {
-		List<Diem> diems = new ArrayList<Diem>();
-		diems = diemRepository.findAll();
-		model.addAttribute("pointList", diems);
-		return "Point";
-	}
-
-	@RequestMapping(value = "/point", method = RequestMethod.POST)
-	public String getPoint() {
-		return "Point";
-	}
-
->>>>>>> 64555b52f96d5c633a26c83a6d5ea68aa091ab16
 }
